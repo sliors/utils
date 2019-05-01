b656557e4df2778fa94cdaefcf5cf7dd8c8f7dc4
root <root@7470-171523-1.imperva.local>
2019-05-02 00:31:57 +0300

    Added a catch for exception of type "unrarlib.UnrarException" that can be thrown if the rar file is corrupted

--- extract_recursive_multi_rar.py
+++ extract_recursive_multi_rar.py
@@ -13,6 +13,7 @@ from pathlib import Path
 import sys
 import os
 from unrar import rarfile
+from unrar import unrarlib
 
 def purge(dir, pattern):
     for f in os.listdir(dir):
@@ -26,28 +27,31 @@ for path in pathlist:
 
     if path.is_file() == False:
         continue
-    
+
     # example: path = dir1/dir2/fileA.part1.rar
-    
+
     # example: file_part1_name = fileA.part1.rar
     file_part1_name = path.name
     # example: parent_path = dir1/dir2
     parent_path = path.parent
-    
+
     matchObj = re.match( r'(.*).part0*1.rar', file_part1_name, re.M|re.I)
     if not matchObj:
         #print("Failed to find match for: ", file_part1_name)
         continue
-        
+
     # example: file_base = fileA
     file_base = matchObj.group(1)
-    
+
     try:
         print("extracting: ", str(path))
         rar = rarfile.RarFile(str(path))
         rar.extractall(str(parent_path))
     except rarfile.BadRarFile:
-        print("Failed to extract: ", path)
+        print("Failed to extract, bad rar file: ", path)
         continue
-    
+    except unrarlib.UnrarException:
+        print("Failed to extract, unrar exception: ", path)
+        continue
+
     purge(str(parent_path), file_base + '.part[0-9]*.rar')
