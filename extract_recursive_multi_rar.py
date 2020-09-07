dfd3a999f468d7063ccd2b5ff2bbc04e746717b4
Lior Shetrit <sliors@gmail.com>
2020-09-07 17:08:19 +0300

    Added support for providing passwords for the rars using -pw flag, e.g., -pw password1 -pw password

--- extract_recursive_multi_rar.py
+++ extract_recursive_multi_rar.py
@@ -9,9 +9,8 @@
 # 2. sudo apt install python3-pip
 # 3. sudo pip3 install unrar
 # 4. unrar_tgz_url=http://www.rarlab.com/rar/unrarsrc-5.9.4.tar.gz && wget $unrar_tgz_url && tar xvzf $unrar_tgz_url && cd unrar && make lib && sudo make install-lib
-# 4.a. Latest unrar source version is in https://www.rarlab.com/rar_add.htm under "UnRAR source"
+# 4.a. Latest unrar source version is in  https://www.rarlab.com/rar_add.htm under "UnRAR source"
 # 5. Add "export UNRAR_LIB_PATH=/usr/lib/libunrar.so" to "/etc/profile"
-
 
 import re
 from pathlib import Path
@@ -19,6 +18,8 @@ import sys
 import os
 from unrar import rarfile
 from unrar import unrarlib
+import argparse
+
 
 def purge(dir, pattern):
     for f in os.listdir(dir):
@@ -26,6 +27,17 @@ def purge(dir, pattern):
             print("deleting: ", os.path.join(dir, f))
             os.remove(os.path.join(dir, f))
 
+
+parser = argparse.ArgumentParser(description='Recursively extract multi rar files')
+parser.add_argument('-pw', action='append', help='rar password')
+args = parser.parse_args()
+
+print('Password list: ' + str(args.pw))
+
+if args.pw is not None:
+    args.pw.insert(0, '')
+else:
+    args.pw = ['']
 
 pathlist = Path(".").glob('**/*.part*1.rar')
 for path in pathlist:
@@ -48,15 +60,32 @@ for path in pathlist:
     # example: file_base = fileA
     file_base = matchObj.group(1)
 
-    try:
-        print("extracting: ", str(path))
-        rar = rarfile.RarFile(str(path))
-        rar.extractall(str(parent_path))
-    except rarfile.BadRarFile:
-        print("Failed to extract, bad rar file: ", path)
-        continue
-    except unrarlib.UnrarException:
-        print("Failed to extract, unrar exception: ", path)
-        continue
+    is_extracted = bool(False)
 
-    purge(str(parent_path), file_base + '.part[0-9]*.rar')
+    for pw in args.pw:
+
+        try:
+            #print("Extracting: ", str(path))
+
+            #with rarfile.RarFile(filename=str(path), pwd='snahp.it') as rar:
+            with rarfile.RarFile(filename=str(path), pwd=pw) as rar:
+                print('Extracting: ' + str(path) + ' with password: ' + pw)
+                rar.extractall(path=str(parent_path))
+                is_extracted = bool(True)
+        except rarfile.BadRarFile:
+            print("Failed to extract, bad rar file: ", path)
+            continue
+        except unrarlib.MissingPassword:
+            print("Missing password")
+            continue
+        except unrarlib.UnrarException:
+            print("Failed to extract, unrar exception, path: ", path)
+            continue
+        except RuntimeError as e:
+            print('Failed to extract, path: ' + str(path) + ', exception: ' + str(e))
+            #print("Runtime error, path: ", path, ", exception: " )
+            continue
+
+    if is_extracted:
+        print('Successfully extracted ' + str(path))
+        purge(str(parent_path), file_base + '.part[0-9]*.rar')
